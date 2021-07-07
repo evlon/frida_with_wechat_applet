575572226fa5bc7e2855125a809148584f4737ac
evlon <liamgnc@gmail.com>
2021-07-07 16:19:52 +0800

    Update hook.js
    动态查找地址

--- hook.js
+++ hook.js
@@ -1,18 +1,12 @@
 /*
  * @name 微信小程序PC版 wxapkg提取
- * @author kksanyu
- * @url https://github.com/kksanyu/frida_with_wechat_applet
- */
-
+ * @author 代码果 基于kksanyu版修改
+*/
 var baseAddr = Module.findBaseAddress('WeChatAppHost.dll');
 console.log('WeChatAppHost.dll baseAddr: ' + baseAddr);
-
-if (baseAddr) {
-
-    // EncryptBufToFile函数偏移地址, 从DLL中查看
-    var EncryptBufToFile = baseAddr.add(0x1800F);
+if (baseAddr) {    
+	var EncryptBufToFile = Module.findExportByName('WeChatAppHost.dll','EncryptBufToFile'); //原文链接：https://blog.csdn.net/m0_46243789/article/details/111591100
     console.log('EncryptBufToFile 函数地址: ' + EncryptBufToFile);
-
     // HOOK函数, 监听参数
     Interceptor.attach(EncryptBufToFile, {
         onEnter: function (args) {
@@ -25,20 +19,18 @@ if (baseAddr) {
         },
         onLeave: function (retval) {
             console.log('文件解密成功', this.apkgFilePath);
-
             // 将文件替换为未加密的wxapkg包
             var f = new File(this.apkgFilePath, 'wb');
             f.write(this.originalData);
             f.flush();
             f.close();
-
             // 释放内存
             delete this.appId;
             delete this.apkgFilePath;
             delete this.originalData;
         }
-    });
-    
+    });    
 } else {
     console.log('WeChatAppHost.dll 模块未加载, 请先打开界面中的小程序面板');
+
 }
